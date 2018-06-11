2d1109a17c11ec54f61eb2b332f3ec7dccbe18a1
Monkeli-Fred DILAPISHO <mdilapi@e4r13p8.wethinkcode.co.za>
2018-06-11 16:37:54 +0200

    Passes the entire 42FileChecker

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -23,7 +23,7 @@ char	*ft_strncpy(char *s1, const char *s2, size_t n)
 		s1[i] = s2[i];
 		i++;
 	}
-	if (s2[i] == '\0')
+	if (s2[i] == '\0' && i < n)
 		s1[i] = '\0';
 	while (i < n)
 		s1[i++] = '\0';
