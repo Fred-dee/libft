f7c2649a1261758b4e007134a27a6e5581e1b5fe
Fred Dilapisho <fred.dilapisho@gmail.com>
2018-07-24 12:47:51 +0200

    Update ft_putendl_clr.c

--- ft_putendl_clr.c
+++ ft_putendl_clr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-include "libft.h"
+#include "libft.h"
 
 void	ft_putendl_clr(char const *clr, char const *str)
 {
