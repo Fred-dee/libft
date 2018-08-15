e6910b93b9407361681d2cbd37d503c67ec09890
Fred-Dee <fred.dilapisho@gmail.com>
2018-08-15 11:13:29 +0200

    fixed issues resulting from incorrect sizeof

--- graph_src/ft_graph_genmatrices.c
+++ graph_src/ft_graph_genmatrices.c
@@ -12,6 +12,40 @@
 
 #include <graph.h>
 
+static void compile_edges(t_graph *g)
+{
+	t_list		*outer;
+	t_list		*inner;
+	t_vertex	*tmp;
+
+	if(g != NULL && g->adj_matrix != NULL)
+	{
+		outer = g->vertices;
+		while (outer != NULL)
+		{
+			tmp = (t_vertex *)outer->content;
+			if (tmp->neighbours != NULL)
+			{
+				inner = tmp->neighbours;
+				while (inner != NULL)
+				{
+					if(tmp->number == *(int *)inner->content)
+					{
+						g->adj_matrix[tmp->number][tmp->number]++;
+					}
+					else
+					{
+						g->adj_matrix[tmp->number][*(int *)inner->content] = 1;
+						g->adj_matrix[*(int *)inner->content][tmp->number] = 1;
+					}
+					inner = inner->next;
+				}
+			}
+			outer = outer->next;
+		}
+	}
+}
+
 int	ft_graph_genmatrices(t_graph *g)
 {
 	int	i;
@@ -33,6 +67,7 @@ int	ft_graph_genmatrices(t_graph *g)
 				g->adj_matrix[i][j++] = 0;
 			i++;
 		}
+		compile_edges(g);
 	}
 	return (TRUE);
 }
